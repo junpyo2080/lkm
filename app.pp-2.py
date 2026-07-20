439997080dda803a1d153a784124f1630acd0821
junpyo2080 <sdr4627@sonline20.sen.go.kr>
2026-07-20 13:45:29 +0900

    Update app.pp-2.py

--- app.pp-2.py
+++ app.pp-2.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.title("카운터 앱")
 if 'count' not in st.session_state:
-    st.session_state.count = 글자
+    st.session_state.count = 숫자
 if st.button("증가"):
-    st.session_state.count +=임경미
-st.markdown(f"## 현재 글자: `{st.session_state.count}`")
+    st.session_state.count +=1
+st.markdown(f"## 현재 숫자: `{st.session_state.count}`")
